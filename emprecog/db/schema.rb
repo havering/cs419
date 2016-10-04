d30a4322b834562634867fadaa032af2442059d3
Diana O'Haver <diohaver@Dianas-MacBook-Pro.local>
2016-10-03 21:42:37 -0700

    Added home index

--- emprecog/db/schema.rb
+++ emprecog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161004033625) do
+ActiveRecord::Schema.define(version: 20161004035810) do
 
   create_table "awards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "type"
@@ -24,9 +24,9 @@ ActiveRecord::Schema.define(version: 20161004033625) do
     t.string   "username"
     t.string   "password"
     t.binary   "signature",  limit: 65535
-    t.string   "role"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.string   "role",                     default: "user"
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
   end
 
 end
