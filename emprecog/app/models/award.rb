a6253c4ac67c540cb625acb985ab6b4704adafbc
Diana O'Haver <diohaver@Dianas-MacBook-Pro.local>
2016-10-12 21:05:40 -0700

    Additional enhancements to form - added award type plus cleaned up granted

--- emprecog/app/models/award.rb
+++ emprecog/app/models/award.rb
@@ -1,2 +1,10 @@
 class Award < ApplicationRecord
+
+  VALID_AWARDS = ["Employee of the Month", "Duct Tape", "Eye of the Storm", "Swiss Army Knife",
+                  "Running with the Bulls", "Appreciation"].freeze
+
+  def self.get_awards
+    return VALID_AWARDS
+  end
+
 end
