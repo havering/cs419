695118f726ede892fcefe0fb0d4409a67b9a593e
Diana O'Haver <diohaver@Dianas-MacBook-Pro.local>
2016-11-08 10:39:29 -0800

    Email is working - removed extraneous puts

--- emprecog/app/controllers/awards_controller.rb
+++ emprecog/app/controllers/awards_controller.rb
@@ -40,9 +40,7 @@ class AwardsController < ApplicationController
         # if it saves, then send off an email about it
         mail = AwardMailer.award_email(user.first, @award.granted, current_user, current_user.signature, @award)
         results = mail.deliver_now
-        puts "$" * 50
-        puts "results is: #{results}"
-        puts "$" * 50
+
         format.html { redirect_to @award, notice: 'Award was successfully created.' }
         format.json { render :show, status: :created, location: @award }
       else
